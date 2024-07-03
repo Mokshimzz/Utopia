1083bfd143d8be175ac78c313435a4cc3af05b8c
mokshiz <mokshitacollege@gmail.com>
2024-07-02 22:16:56 -0700

    all pages

--- script.js
+++ script.js
@@ -21,6 +21,68 @@ document.addEventListener("DOMContentLoaded", (event) => {
             // end:"+=100vh"
         }
     })
+
+    gsap.to("#details", {
+        scrollTrigger:{
+            trigger:"#details",
+            pin:"#navbar",
+            scrub:10,
+            pinSpacing:false,
+            // end:"+=100vh"
+        }
+    })
 });
 
+let details = document.querySelector("#details")
+let contact = document.querySelector("#contact-cont")
+let committees = document.querySelector("#committees-cont")
+let info = document.querySelector("#info")
+let rest = document.querySelector(".rest")
+
+function viewInfo(){
+
+    info.scrollIntoView()
+
+    // if(!contact.classList.contains("hidden")){
+    //     contact.classList.add("hidden")
+    // }
+    // if(!committees.classList.contains("hidden")){
+    //     committees.classList.add("hidden")
+    // }
+
+    // info.classList.remove("hidden")
+    // rest.classList.remove("hidden")
+}
+function viewCommittees(){
+
+    committees.scrollIntoView()
+    // if(!contact.classList.contains("hidden")){
+    //     contact.classList.add("hidden")
+    // }
+    // if(!info.classList.contains("hidden")){
+    //     info.classList.add("hidden")
+    // }
+    // if(!rest.classList.contains("hidden")){
+    //     rest.classList.add("hidden")
+    // }
+
+    // committees.classList.remove("hidden")
+    // details.style.height = (details.style.height - (40 * committees.offsetHeight)) + "px"
+}
+function viewContact(){
+
+    contact.scrollIntoView()
+    // if(!info.classList.contains("hidden")){
+    //     info.classList.add("hidden")
+    // }
+    // if(!rest.classList.contains("hidden")){
+    //     rest.classList.add("hidden")
+    // }
+    // if(!committees.classList.contains("hidden")){
+    //     committees.classList.add("hidden")
+    // }
+
+    // contact.classList.remove("hidden")
+
+}
 // gsap.registerPlugin(ScrollTrigger)
